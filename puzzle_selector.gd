70c61d13681ac135281b615bddbeb4d24b096520
Félix Mariotto <felix.mariotto@gmail.com>
2025-03-25 22:23:34 +0100

    layout the puzzle images, and listen for clicks

--- puzzle_selector.gd
+++ puzzle_selector.gd
@@ -7,4 +7,13 @@ func _ready() -> void:
 		var textureRect = TextureRect.new()
 		textureRect.texture = image
 		textureRect.size = Vector2( 20.0, 20.0 )
+		textureRect.expand_mode = textureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
 		add_child( textureRect )
+		textureRect.connect( "gui_input", _on_interact_texture_rect.bind(image) )
+
+func _on_interact_texture_rect(event, image):
+	if event is InputEventMouseButton and event.pressed:
+		restart_puzzle_with_texture(image)
+
+func restart_puzzle_with_texture(image):
+	print(image)
